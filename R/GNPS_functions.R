f2a33ffbfba9b8159b8322e75f9c9b8e83e20199
Asker Brejnrod <askerbrejnrod@gmail.com>
2018-08-15 20:51:44 +0200

    slight refactor

--- R/GNPS_functions.R
+++ R/GNPS_functions.R
@@ -1,6 +1,5 @@
 library(reshape2)
 library(igraph)
-
 #' download_GNPS
 #'
 #' @param ID The GNPS id to retrieve
@@ -13,14 +12,12 @@ library(igraph)
 download_GNPS <- function(ID, dir){
   #TODO: handle several datasets extracted in the same dir
   url <- paste0('https://gnps.ucsd.edu/ProteoSAFe/DownloadResult?task=', ID, "&view=download_cluster_buckettable")
-  print(paste(ID, dir, url))
   temp_gnps <- paste0(dir, "/", ID,"_GNPS.zip")
-  #f = CFILE(file = temp_gnps , mode = "wb")
-  #results <- postForm(url, dummy="", binary=T, .opts = curlOptions(writedata = f@ref))
-  #print(paste("Download status", results))
-  #RCurl::close(f)
-  system(paste0('curl -d "" \"', url, '\" -o ', temp_gnps), show.output.on.console = F)
-  print(paste0('curl -d "" ', url, ' -o ', temp_gnps))
+  f = CFILE(file = temp_gnps , mode = "wb")
+  results <- postForm(url, .opts = curlOptions(writedata = f@ref), .params = list(dummy =""),binary=T,style ="post")
+  print(paste("Download status", results))
+  RCurl::close(f)
+  #system(paste0('curl -d "" \"', url, '\" -o ', temp_gnps), show.output.on.console = F)
   unzip(temp_gnps, exdir = paste(dir,"/",ID,sep=""))
   files <- Sys.glob(file.path(paste(dir,"/",ID,sep=""), "*"))
   netattr <- list.files(grep("clusterinfosummarygroup_attributes_withIDs_withcomponentID", files, value = T), full.names = T)
@@ -30,7 +27,13 @@ download_GNPS <- function(ID, dir){
   return(list(buckettable = buckettable, edges = edges, attr = netattr, ids = ids))
 }
 
-
+prepare_css <- function(edge_path){
+  csslong <- read.table(edge_path, header=T)
+  g <- igraph::graph.data.frame(csslong, directed=FALSE)
+  css <- igraph::get.adjacency(g, attr="Cosine", sparse=FALSE)
+  diag(css) <- 1
+  return(css)
+}
 
 #' prepare_GNPS
 #'
@@ -41,14 +44,10 @@ download_GNPS <- function(ID, dir){
 #' @export
 #'
 #' @examples prepare_GNPS("0310e20491314ddbbf12d56b592548b4")
+
 prepare_GNPS <- function(ID, dir = ".", select = T){
   gnps <- download_GNPS(ID, dir)
   features <- read.table(gnps$buckettable, sep = "\t", header=T, row.names=1, comment.char="")
-  csslong <- read.table(gnps$edges, header=T)
-  
-  g <- igraph::graph.data.frame(csslong, directed=FALSE)
-  css <- igraph::get.adjacency(g, attr="Cosine", sparse=FALSE)
-  diag(css) <- 1
-
+  css <- prepare_css(gnps$edges)
   return(list(features = features, css = css))
 }
